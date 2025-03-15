8f7cd69c4091bc4df921c6f33591161fa5b2d772
Thomasrnd <thomasrnd02@gmail.com>
2025-03-15 22:34:46 +0800

    try cord

--- src/config/app.js
+++ src/config/app.js
@@ -30,6 +30,7 @@ const config = {
     origin: process.env.CORS_ORIGIN || '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   },
 
   // Validation
